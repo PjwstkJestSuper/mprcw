8996c3f834e1b2ea75ba922b6ebe06aacc354caf
s27091 <s27091@pjwstk.edu.pl>
2023-10-09 16:38:16 +0200

    projekt

---
+++ src/main/java/com/example/mprcw/MprcwApplication.java
@@ -0,0 +1,13 @@
+package com.example.mprcw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MprcwApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MprcwApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mprcw/MprcwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mprcw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MprcwApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
